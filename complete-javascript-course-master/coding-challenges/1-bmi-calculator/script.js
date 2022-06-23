9f6b81ea690aad10120459c839ade2135d89f9a6
Otto William Peyer IV <opeyer@gmail.com>
2022-06-23 16:01:58 -0500

    Additional polishing work on Coding Challenge #1.

--- complete-javascript-course-master/coding-challenges/1-bmi-calculator/script.js
+++ complete-javascript-course-master/coding-challenges/1-bmi-calculator/script.js
@@ -6,20 +6,17 @@ const markHeight1 = 1.69;
 const johnMass1 = 92;
 const johnHeight1 = 1.95;
 let markBMI1 = markMass1 / markHeight1 ** 2;
-console.log(markBMI1);
-let johnBMI1 = johnMass1 / johnHeight1 ** 2;
-console.log(johnBMI1);
+let johnBMI1 = johnMass1 / (johnHeight1 * johnHeight1);
 
 const markMass2 = 95;
 const markHeight2 = 1.88;
 const johnMass2 = 85;
 const johnHeight2 = 1.76;
 let markBMI2 = markMass2 / markHeight2 ** 2;
-console.log(markBMI2);
-let johnBMI2 = johnMass2 / johnHeight2 ** 2;
-console.log(johnBMI2);
+let johnBMI2 = johnMass2 / (johnHeight2 * johnHeight2);
 
 var markHigherBMI1 = markBMI1 > johnBMI1;
 var markHigherBMI2 = markBMI2 > johnBMI2;
-console.log(markHigherBMI1);
-console.log(markHigherBMI2);
+
+console.log(markBMI1, johnBMI1, markHigherBMI1);
+console.log(markBMI2, johnBMI2, markHigherBMI2);
